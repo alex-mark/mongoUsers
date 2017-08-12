748336d12dcd74b62619fe11389967040bfc9ba7
alex-mark <alexndr86@gmail.com>
2017-08-12 22:06:41 +0300

    create_test works

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,30 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-  .once('open', () => console.log('Good to go'))
-  .on('error', (error) => console.warn('Warning', error));
+mongoose.Promise = global.Promise;
+
+before((done) => {
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection
+    .once('open', () => { done(); })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
+
+// beforeEach((done) => {
+//   const { users, comments, blogposts } = mongoose.connection.collections;
+//   users.drop(() => {
+//     comments.drop(() => {
+//       blogposts.drop(() => {
+//         done();
+//       });
+//     });
+//   });
+// });
+
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    // Ready to run the next test!
+    done();
+  });
+})
